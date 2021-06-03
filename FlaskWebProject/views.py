ad0d5b640436b6fb0cb6c71b5d22499fda4ceef6
kibablu <kibablu16@gmail.com>
2021-06-03 09:50:43 +0300

    Add successful and unsuccessul login attempts

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -65,9 +65,11 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning("Invalid login attempt: Oops try again later...")
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info("admin logged in successfully,")
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("home")
@@ -106,6 +108,7 @@ def authorized():
 @app.route("/logout")
 def logout():
     logout_user()
+    app.logger.info("admin logged out successfully, sad to see you go")
     if session.get("user"):  # Used MS Login
         # Wipe out user and its token cache from session
         session.clear()
